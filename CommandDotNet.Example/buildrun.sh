e06524093595b57e080684604b986862947ba8e0
Bilal Fazlani <bilal_fazlani@external.mckinsey.com>
2017-12-16 16:43:11 +0530

    Improved build script

--- CommandDotNet.Example/buildrun.sh
+++ CommandDotNet.Example/buildrun.sh
@@ -1,1 +1,4 @@
-dotnet build && dotnet bin/Debug/netcoreapp2.0/CommandDotNet.Example.dll $1 $2 $3 $4 $5 $6 $7
+#!/bin/bash -e
+dotnet build
+clear
+dotnet bin/Debug/netcoreapp2.0/CommandDotNet.Example.dll $1 $2 $3 $4 $5 $6 $7
