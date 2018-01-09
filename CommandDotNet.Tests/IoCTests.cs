5da6d4221ecad59fa34485d3bc4415cbb377d8e9
Bilal Fazlani <bilal_fazlani@external.mckinsey.com>
2018-01-09 12:56:52 +0530

    Added tests for nested app using IoC

---
+++ CommandDotNet.Tests/IoCTests.cs
@@ -0,0 +1,55 @@
+﻿using Autofac;
+using CommandDotNet.Attributes;
+using CommandDotNet.IoC.Autofac;
+using FluentAssertions;
+using Xunit;
+
+namespace CommandDotNet.Tests
+{
+    public class IoCTests
+    {
+        [Fact]
+        public void CanResolveDependencyInNestedCommand()
+        {
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterType<Service>().As<IService>();
+            IContainer container = containerBuilder.Build();
+
+            AppRunner<NestedCommandApp> appRunner = new AppRunner<NestedCommandApp>().UseAutofac(container);
+                
+            appRunner.Run("InnerApp", "-a", "3", "Process").Should().Be(7);
+        }
+    }
+
+    public class NestedCommandApp
+    {
+        public class InnerApp
+        {
+            private readonly int _additionFactor;
+            public IService Service { get; set; }
+
+            public InnerApp([Option(ShortName = "a")]int additionFactor)
+            {
+                _additionFactor = additionFactor;
+            }
+            
+            public int Process()
+            {
+                return Service.GetValue() + _additionFactor;
+            }
+        }
+    }
+
+    public interface IService
+    {
+        int GetValue();
+    }
+
+    public class Service : IService
+    {
+        public int GetValue()
+        {
+            return 4;
+        }
+    }
+}
