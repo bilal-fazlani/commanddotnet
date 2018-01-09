1f6551228dbf2eaa69dd02a66f9396f99b809d2c
Bilal Fazlani <bilal_fazlani@external.mckinsey.com>
2018-01-09 11:54:36 +0530

    Extracted IoC in separate nuget packages

--- CommandDotNet.Example/Program.cs
+++ CommandDotNet.Example/Program.cs
@@ -1,29 +1,10 @@
-﻿using System;
-using Autofac;
-using CommandDotNet.IoC.Autofac;
-using CommandDotNet.IoC.MicrosoftDependencyInjection;
-using CommandDotNet.Models;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace CommandDotNet.Example
+﻿namespace CommandDotNet.Example
 {
     class Program
     {
         static int Main(string[] args)
         {
-            IServiceCollection serviceCollection = new ServiceCollection();
-            serviceCollection.AddSingleton<IService, Service>();
-
-            IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
-
-            IService service = serviceProvider.GetService<IService>();
-
-            Console.WriteLine(service.value);
-
-            AppRunner<MyApplication> appRunner = new AppRunner<MyApplication>()
-                //.UseAutofac(container)
-                .UseMicrosoftDependencyInjection(serviceProvider);
-            
+            AppRunner<MyApplication> appRunner = new AppRunner<MyApplication>();
             return appRunner.Run(args);
         }
     }
