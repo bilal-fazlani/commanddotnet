fe01085570b971cd94864cab86fbf25398da24d9
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-01-20 23:22:56 +0530

    added logic to publish to nuget server

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
+#PACKAGE
 dotnet pack \
 -o output \
 -c Release \
 CommandDotNet/CommandDotNet.csproj \
 /p:Version=$TRAVIS_TAG
 
-
+#PUBLISH TO NUGET
+dotnet nuget push -k $NUGET_API_KEY CommandDotNet.$TRAVIS_TAG.nupkg
