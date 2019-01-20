6553c09b86cadb4c73fc54625ba7294aa58931dc
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-01-20 15:58:10 +0530

    added DebugType & DebugSymbols in csproj

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-dotnet pack -o output -c Release --include-symbols --include-source CommandDotNet/CommandDotNet.csproj /p:Version=%APPVEYOR_BUILD_VERSION%
+dotnet pack ^
+ -o output ^
+ -c Release^
+ CommandDotNet/CommandDotNet.csproj^
+ /p:Version=%APPVEYOR_BUILD_VERSION%
