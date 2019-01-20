da5e0b8d4123b552466454d2c44d9367e86996b8
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-01-20 12:08:28 +0530

    fix build scripts

--- build.bat
+++ build.bat
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash -e
-
 dotnet pack -o output -c Release --include-symbols -p:SymbolPackageFormat=snupkg --include-source CommandDotNet/CommandDotNet.csproj /p:Version=%APPVEYOR_BUILD_VERSION%
--- test.bat
+++ test.bat
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash -e
-
 dotnet test CommandDotNet.Tests/CommandDotNet.Tests.csproj
