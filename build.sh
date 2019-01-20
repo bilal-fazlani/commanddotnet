d725665d28c18c8cfdf3737c6494e9aca65f5e91
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-01-20 13:06:22 +0530

    travis-fix: remove appveyor vesion number from build

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #! /bin/bash -e
 
-dotnet pack -o output -c Release --include-symbols -p:SymbolPackageFormat=snupkg --include-source CommandDotNet/CommandDotNet.csproj /p:Version=%APPVEYOR_BUILD_VERSION%
+dotnet pack \
+-o output \
+-c Release \
+--include-symbols \
+-p:SymbolPackageFormat=snupkg \
+--include-source \
+CommandDotNet/CommandDotNet.csproj \
+/p:Version=1.0.0-SNAPSHOT
