19583f9f7893d1456dbc35c817c44a2e47df96d0
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-02-09 18:40:49 +0530

    fix deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ CommandDotNet/CommandDotNet.csproj \
 /p:Version=$TRAVIS_TAG
 
 #PUBLISH TO NUGET
-dotnet nuget push -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY_COMMANDDOTNET CommandDotNet.$TRAVIS_TAG.nupkg
+dotnet nuget push -s https://api.nuget.org/v3/index.json -k $NUGET_API_KEY_COMMANDDOTNET output/CommandDotNet.$TRAVIS_TAG.nupkg
