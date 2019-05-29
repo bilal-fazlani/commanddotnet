ea819a4bf6f103dcb61d3e104b21e2d4933fb5a0
drew.burlingame <drew.burlingame@valantmed.com>
2019-05-29 12:07:36 +0100

    add bdd test for optional params of class & struct type

--- CommandDotNet.Tests/BddTests/TestScenarios/OptionalParamsScenarios.cs
+++ CommandDotNet.Tests/BddTests/TestScenarios/OptionalParamsScenarios.cs
@@ -1,3 +1,4 @@
+using System;
 using CommandDotNet.Attributes;
 using CommandDotNet.Tests.BddTests.Framework;
 using CommandDotNet.Tests.Utils;
@@ -18,7 +19,7 @@ namespace CommandDotNet.Tests.BddTests.TestScenarios
 
 Arguments:
 
-  arg1    <TEXT>
+  stringArg    <TEXT>
 
 
 Options:
@@ -36,7 +37,7 @@ Options:
 
 Arguments:
 
-  arg1    <TEXT>    [some-default]
+  stringArg    <TEXT>    [some-default]
 
 
 Options:
@@ -48,23 +49,153 @@ Options:
                 new Given<OptionalParamsApp>("executes with value passed to nulled string")
                 {
                     WhenArgs = "NullString some-value",
-                    Then = {Outputs = {new RunResults {Arg1 = "some-value"}}}
+                    Then = {Outputs = {new RunResults {StringArg = "some-value"}}}
                 },
                 new Given<OptionalParamsApp>("executes with no value passed to nulled string")
                 {
                     WhenArgs = "NullString",
-                    Then = {Outputs = {new RunResults()}}
+                    Then = {Outputs = {new RunResults{StringArg = null}}}
                 },
                 new Given<OptionalParamsApp>("executes with value passed to defaulted string")
                 {
                     WhenArgs = "DefaultString some-value",
-                    Then = {Outputs = {new RunResults {Arg1 = "some-value"}}}
+                    Then = {Outputs = {new RunResults {StringArg = "some-value"}}}
                 },
                 new Given<OptionalParamsApp>("executes with no value passed to defaulted string")
                 {
                     WhenArgs = "DefaultString",
-                    Then = {Outputs = {new RunResults{Arg1 = "some-default"}}}
-                }
+                    Then = {Outputs = {new RunResults{StringArg = "some-default"}}}
+                },
+                new Given<OptionalParamsApp>("help displays args defined with nulled object")
+                {
+                    WhenArgs = "NullObject -h",
+                    Then =
+                    {
+                        Help = @"Usage: dotnet testhost.dll NullObject [arguments] [options]
+
+Arguments:
+
+  uriArg    <URI>
+
+
+Options:
+
+  -h | --help
+  Show help information"
+                    }
+                },
+                new Given<OptionalParamsApp>("help displays args defined with nulled object")
+                {
+                    WhenArgs = "NullObject -h",
+                    Then =
+                    {
+                        Help = @"Usage: dotnet testhost.dll NullObject [arguments] [options]
+
+Arguments:
+
+  uriArg    <URI>
+
+
+Options:
+
+  -h | --help
+  Show help information"
+                    }
+                },
+                new Given<OptionalParamsApp>("executes with value passed to nulled object")
+                {
+                    WhenArgs = "NullObject http://google.com",
+                    Then = {Outputs = {new RunResults {UriArg = new Uri("http://google.com")}}}
+                },
+                new Given<OptionalParamsApp>("executes with no value passed to nulled object")
+                {
+                    WhenArgs = "NullObject",
+                    Then = {Outputs = {new RunResults{UriArg = null}}}
+                },
+                new Given<OptionalParamsApp>("help displays args defined with defaulted int")
+                {
+                    WhenArgs = "DefaultInt -h",
+                    Then =
+                    {
+                        Help = @"Usage: dotnet testhost.dll DefaultInt [arguments] [options]
+
+Arguments:
+
+  intArg    <NUMBER>    [1]
+
+
+Options:
+
+  -h | --help
+  Show help information"
+                    }
+                },
+                new Given<OptionalParamsApp>("executes with value passed to defaulted int")
+                {
+                    WhenArgs = "DefaultInt 5",
+                    Then = {Outputs = {new RunResults {IntArg = 5}}}
+                },
+                new Given<OptionalParamsApp>("executes with no value passed to defaulted int")
+                {
+                    WhenArgs = "DefaultInt",
+                    Then = {Outputs = {new RunResults {IntArg = 1}}}
+                },
+                new Given<OptionalParamsApp>("help displays args defined with nulled int?")
+                {
+                    WhenArgs = "NullNullableInt -h",
+                    Then =
+                    {
+                        Help = @"Usage: dotnet testhost.dll NullNullableInt [arguments] [options]
+
+Arguments:
+
+  intArg    <NUMBER>
+
+
+Options:
+
+  -h | --help
+  Show help information"
+                    }
+                },
+                new Given<OptionalParamsApp>("help displays args defined with defaulted int?")
+                {
+                    WhenArgs = "DefaultNullableInt -h",
+                    Then =
+                    {
+                        Help = @"Usage: dotnet testhost.dll DefaultNullableInt [arguments] [options]
+
+Arguments:
+
+  intArg    <NUMBER>    [1]
+
+
+Options:
+
+  -h | --help
+  Show help information"
+                    }
+                },
+                new Given<OptionalParamsApp>("executes with value passed to nulled int?")
+                {
+                    WhenArgs = "NullNullableInt 5",
+                    Then = {Outputs = {new RunResults {IntArg = 5}}}
+                },
+                new Given<OptionalParamsApp>("executes with no value passed to nulled int?")
+                {
+                    WhenArgs = "NullNullableInt",
+                    Then = {Outputs = {new RunResults {IntArg = null}}}
+                },
+                new Given<OptionalParamsApp>("executes with value passed to defaulted int?")
+                {
+                    WhenArgs = "DefaultNullableInt 5",
+                    Then = {Outputs = {new RunResults {IntArg = 5}}}
+                },
+                new Given<OptionalParamsApp>("executes with no value passed to defaulted int?")
+                {
+                    WhenArgs = "DefaultNullableInt",
+                    Then = {Outputs = {new RunResults {IntArg = 1}}}
+                },
             };
 
         private class OptionalParamsApp
@@ -72,20 +203,42 @@ Options:
             [InjectProperty]
             public TestOutputs TestOutputs { get; set; }
 
-            public void NullString(string arg1 = null)
+            public void NullString(string stringArg = null)
             {
-                TestOutputs.Capture(new RunResults{Arg1 = arg1});
+                TestOutputs.Capture(new RunResults{StringArg = stringArg});
             }
 
-            public void DefaultString(string arg1 = "some-default")
+            public void DefaultString(string stringArg = "some-default")
             {
-                TestOutputs.Capture(new RunResults { Arg1 = arg1 });
+                TestOutputs.Capture(new RunResults { StringArg = stringArg });
+            }
+
+            public void NullObject(Uri uriArg = null)
+            {
+                TestOutputs.Capture(new RunResults { UriArg = uriArg });
+            }
+
+            public void NullNullableInt(int? intArg = null)
+            {
+                TestOutputs.Capture(new RunResults { IntArg = intArg });
+            }
+
+            public void DefaultNullableInt(int? intArg = 1)
+            {
+                TestOutputs.Capture(new RunResults { IntArg = intArg });
+            }
+
+            public void DefaultInt(int intArg = 1)
+            {
+                TestOutputs.Capture(new RunResults { IntArg = intArg });
             }
         }
 
         private class RunResults
         {
-            public string Arg1 { get; set; }
+            public string StringArg { get; set; }
+            public Uri UriArg { get; set; }
+            public int? IntArg { get; set; }
         }
     }
 }
