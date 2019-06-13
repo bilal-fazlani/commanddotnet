6c99cbe5ebc6bd023d48d1bab89d23d888b199d9
drew.burlingame <drew.burlingame@valantmed.com>
2019-06-13 08:13:59 +0100

    stop overwriting singleton console

--- CommandDotNet.Tests/TestBase.cs
+++ CommandDotNet.Tests/TestBase.cs
@@ -1,25 +1,15 @@
-﻿using System;
-using CommandDotNet.Tests.ScenarioFramework;
-using CommandDotNet.Tests.Utils;
+﻿using CommandDotNet.Tests.ScenarioFramework;
 using Xunit.Abstractions;
 
 namespace CommandDotNet.Tests
 {
     public class TestBase
     {
-        protected readonly ITestOutputHelper TestOutputHelper;
-
         private readonly ScenarioVerifier _scenarioVerifier;
 
         protected TestBase(ITestOutputHelper testOutputHelper)
         {
-            TestOutputHelper = testOutputHelper;
             _scenarioVerifier = new ScenarioVerifier(testOutputHelper);
-
-            // WARNING: this doesn't play well with parallelized tests
-            var testConsoleWriter = new TestConsoleWriter(testOutputHelper);
-            Console.SetOut(testConsoleWriter);
-            Console.SetError(testConsoleWriter);
         }
 
         protected void Verify(IScenario scenario)
