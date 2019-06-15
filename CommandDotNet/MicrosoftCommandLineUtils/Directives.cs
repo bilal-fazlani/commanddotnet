ad09d23575a6e523e9cbbc95e019415f3451387c
drew.burlingame <drew.burlingame@valantmed.com>
2019-06-15 14:07:16 +0100

    add [debug] and [parse] directives

--- CommandDotNet/MicrosoftCommandLineUtils/Directives.cs
+++ CommandDotNet/MicrosoftCommandLineUtils/Directives.cs
@@ -3,7 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CommandDotNet.Example
+namespace CommandDotNet.MicrosoftCommandLineUtils
 {
     internal static class Directives
     {
