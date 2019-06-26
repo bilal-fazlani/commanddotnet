f747673537da3dc49e0394d3663f1c8f76b6346a
drew.burlingame <drew.burlingame@valantmed.com>
2019-06-26 12:23:42 +0100

    ensure ArgumentTypeDescriptor errors are consistent
    It was only consistent if SingleValueParser was called
    but SingleValueParser is internal and descriptors are public

---
+++ CommandDotNet/TypeDescriptors/ErrorReportingDescriptor.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandDotNet.Exceptions;
+using CommandDotNet.Models;
+
+namespace CommandDotNet.TypeDescriptors
+{
+    internal class ErrorReportingDescriptor: IArgumentTypeDescriptor, IAllowedValuesTypeDescriptor
+    {
+        private readonly IArgumentTypeDescriptor _innerDescriptor;
+
+        public ErrorReportingDescriptor(IArgumentTypeDescriptor innerDescriptor)
+        {
+            _innerDescriptor = innerDescriptor;
+        }
+
+        public bool CanSupport(Type type)
+        {
+            return _innerDescriptor.CanSupport(type);
+        }
+
+        public string GetDisplayName(ArgumentInfo argumentInfo)
+        {
+            return _innerDescriptor.GetDisplayName(argumentInfo);
+        }
+
+        public object ParseString(ArgumentInfo argumentInfo, string value)
+        {
+            try
+            {
+                return _innerDescriptor.ParseString(argumentInfo, value);
+            }
+            catch (FormatException)
+            {
+                throw new ValueParsingException(
+                    $"'{value}' is not a valid {_innerDescriptor.GetDisplayName(argumentInfo)}");
+            }
+            catch (ArgumentException)
+            {
+                throw new ValueParsingException(
+                    $"'{value}' is not a valid {_innerDescriptor.GetDisplayName(argumentInfo)}");
+            }
+        }
+
+        public IEnumerable<string> GetAllowedValues(ArgumentInfo argumentInfo)
+        {
+            return (_innerDescriptor as IAllowedValuesTypeDescriptor)?.GetAllowedValues(argumentInfo) 
+                   ?? Enumerable.Empty<string>();
+        }
+    }
+}
