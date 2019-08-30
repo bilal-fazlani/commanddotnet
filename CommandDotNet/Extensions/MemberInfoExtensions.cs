7a283820a92ae81a7c786fff6d643e1d94bcacf6
drew.burlingame <drew.burlingame@valantmed.com>
2019-08-30 08:13:09 +0100

    move MemberInfoExtensions to it's own file

---
+++ CommandDotNet/Extensions/MemberInfoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace CommandDotNet.Extensions
+{
+    internal static class MemberInfoExtensions
+    {
+        internal static string FullName(this MemberInfo memberInfo, bool includeNamespace = false) =>
+            includeNamespace
+                ? $"{memberInfo.DeclaringType?.FullName}.{memberInfo.Name}"
+                : $"{memberInfo.DeclaringType?.Name}.{memberInfo.Name}";
+    }
+}
