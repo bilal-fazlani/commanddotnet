c9cec166312a8d236b5ed4d42582ca2010713520
drew.burlingame <drew.burlingame@valantmed.com>
2019-11-08 20:35:03 +0000

    add mkdocs-build.sh and mkdocs-serve.sh
    dev-docker-mkdocs.sh renamed to mkdocs-serve.sh and
    updated for correct use on mac

--- mkdocs-serve.sh
+++ mkdocs-serve.sh
@@ -4,7 +4,7 @@ EXPOSED_PORT=8001
 echo 'Connect your browser to 127.0.0.1:'${EXPOSED_PORT}
 
 # use this line on mac/linux.
-# docker run --rm --name cmdnet-mkdocs-material -it -p ${EXPOSED_PORT}:8000 -v ${PWD}:/docs -v /${PWD}/site:/site squidfunk/mkdocs-material
+# docker run --rm --name mkdocs-material -it -p ${EXPOSED_PORT}:8000 -v ${PWD}:/docs -v squidfunk/mkdocs-material
 
 # use this line on windows to escape the path conversion on windows.  
 # https://stackoverflow.com/questions/50608301/docker-mounted-volume-adds-c-to-end-of-windows-path-when-translating-from-linux
