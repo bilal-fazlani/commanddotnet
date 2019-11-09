838b67e241a69362b8ec7122d2483bbda1c01dde
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2019-11-10 00:40:57 +0530

    script cleanup

--- publish_doc.sh
+++ publish_doc.sh
@@ -1,28 +1,27 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 echo "installing tools"
 pip install mkdocs mkdocs-material pygments pymdown-extensions recommonmark
 echo "installed"
 
-ls -alh
-git worktree list
 git worktree add site gh-pages
 
 echo "generating new documentation"
 mkdocs build
 echo "documentation generated"
 
-# push setup
+echo "commit pages"
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
 git config --global push.default current
-
-echo "publishing to github"
 cd site
 git add --all
 git commit -m "${TRAVIS_COMMIT_MESSAGE}"
+echo "committed pages"
+
+echo "publishing to github"
 git push https://${GITHUB_TOKEN}@github.com/bilal-fazlani/commanddotnet.git
-# git push origin gh-pages
+echo "published"
+
 cd ..
 git worktree remove site
-echo "published"
