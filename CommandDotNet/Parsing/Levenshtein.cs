00f75831f0d54fab32c837602198a0e90e04cc07
Drew Burlingame <drewburlingame@gmail.com>
2020-03-05 03:20:13 +0000

    add Levenshtein & experiment with algos

---
+++ CommandDotNet/Parsing/Levenshtein.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CommandDotNet.Parsing
+{
+    internal static class Levenshtein
+    {
+        //https://www.eximiaco.tech/en/2019/11/17/computing-the-levenshtein-edit-distance-of-two-strings-using-c/
+
+        internal static int ComputeDistance(string first, string second)
+        {
+            if (first.Length == 0)
+            {
+                return second.Length;
+            }
+
+            if (second.Length == 0)
+            {
+                return first.Length;
+            }
+
+            var current = 1;
+            var previous = 0;
+            var r = new int[2, second.Length + 1];
+            for (var i = 0; i <= second.Length; i++)
+            {
+                r[previous, i] = i;
+            }
+
+            for (var i = 0; i < first.Length; i++)
+            {
+                r[current, 0] = i + 1;
+
+                for (var j = 1; j <= second.Length; j++)
+                {
+                    var cost = (second[j - 1] == first[i]) ? 0 : 1;
+                    r[current, j] = Min(
+                        r[previous, j] + 1,
+                        r[current, j - 1] + 1,
+                        r[previous, j - 1] + cost);
+                }
+
+                previous = (previous + 1) % 2;
+                current = (current + 1) % 2;
+            }
+
+            return r[previous, second.Length];
+        }
+
+        private static int Min(int e1, int e2, int e3) =>
+            Math.Min(Math.Min(e1, e2), e3);
+    }
+}
--- CommandDotNet/Parsing/TypoSuggestionsMiddleware.cs
+++ CommandDotNet/Parsing/TypoSuggestionsMiddleware.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CommandDotNet.Execution;
 using CommandDotNet.Extensions;
 using CommandDotNet.Help;
+using CommandDotNet.Rendering;
 using CommandDotNet.Tokens;
 
 namespace CommandDotNet.Parsing
@@ -27,21 +29,27 @@ namespace CommandDotNet.Parsing
                 var command = cpe.Command;
                 var usage = command.GetAppName(ctx.AppConfig.AppSettings.Help) + " " +  command.GetPath();
 
+                var unrecognizedValue = cpe.UnrecognizedArgument.Value;
+
                 switch (cpe.UnrecognizedArgument.TokenType)
                 {
                     case TokenType.Option:
                         @out.WriteLine($"{cpe.UnrecognizedArgument.RawValue} is not an option.  See '{usage} --help'");
                         if (command.Options.Any())
                         {
-                            @out.WriteLine();
-                            @out.WriteLine("The most similar option is");
                             if (cpe.UnrecognizedArgument.OptionTokenType.IsLong)
                             {
-                                command.Options.Where(o => !o.LongName.IsNullOrWhitespace()).ForEach(o => @out.WriteLine($"   --{o.LongName}"));
+                                command.Options
+                                    .Where(o => o.ShowInHelp)
+                                    .Select(o => o.LongName)
+                                    .WriteClosestPossibleMatches("option", unrecognizedValue, "--", ctx.Console);
                             }
                             else
                             {
-                                command.Options.Where(o => o.ShortName != null).ForEach(o => @out.WriteLine($"   -{o.ShortName}"));
+                                command.Options
+                                    .Where(o => o.ShowInHelp)
+                                    .Select(o => o.ShortName.ToString())
+                                    .WriteClosestPossibleMatches("option", unrecognizedValue, "-", ctx.Console);
                             }
                         }
                         break;
@@ -49,9 +57,9 @@ namespace CommandDotNet.Parsing
                         @out.WriteLine($"{cpe.UnrecognizedArgument.RawValue} is not a command.  See '{usage} --help'");
                         if (command.Subcommands.Any())
                         {
-                            @out.WriteLine();
-                            @out.WriteLine("The most similar command is");
-                            command.Subcommands.ForEach(c => @out.WriteLine($"   {c.Name}"));
+                            command.Subcommands
+                                .Select(o => o.Name)
+                                .WriteClosestPossibleMatches("command", unrecognizedValue, null, ctx.Console);
                         }
                         break;
                     case TokenType.Directive:
@@ -63,6 +71,47 @@ namespace CommandDotNet.Parsing
             }
 
             return next(ctx);
+        }
+
+        private static void WriteClosestPossibleMatches(this IEnumerable<string> names, 
+            string title, string unrecognizedValue, string prefix, IConsole console)
+        {
+
+            names = names.Where(n => !n.IsNullOrWhitespace()).ToList();
+            console.Out.WriteLine();
+            console.Out.WriteLine($"The most similar {title} is");
+            names.Select(name => (
+                    name, 
+                    distance: Levenshtein.ComputeDistance(unrecognizedValue, name),
+                    startsWith: GetStartsWithDistance(unrecognizedValue, name),
+                    sameness: GetSamenessDistance(unrecognizedValue, name),
+                    score: 0))
+                .OrderBy(v => v.distance / (v.startsWith + v.sameness + 1))
+                .ThenBy(v => v.distance + -v.sameness)
+                .ForEach(v => console.Out.WriteLine($"   {prefix}{v.name} (score1: {v.distance / (v.startsWith + v.sameness + 1)} score2: {v.distance + -v.sameness})"));
+        }
+
+        private static int GetStartsWithDistance(string first, string second)
+        {
+            for (int i = 0; i < Math.Min(first.Length, second.Length); i++)
+            {
+                if (first[i] != second[i])
+                    return i;
+            }
+
+            return first.Length;
+        }
+
+        private static int GetSamenessDistance(string first, string second)
+        {
+            int same = 0;
+            for (int i = 0; i < Math.Min(first.Length, second.Length); i++)
+            {
+                if (first[i] != second[i])
+                    same++;
+            }
+
+            return same;
         }
     }
 }
