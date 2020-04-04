030f650e213a68f73f751cf0a848e405ff4ac261
Drew Burlingame <drewburlingame@gmail.com>
2020-04-04 02:21:38 +0100

    add math commands to example app

---
+++ CommandDotNet.Example/Commands/Math.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommandDotNet.Rendering;
+
+namespace CommandDotNet.Example.Commands
+{
+    public class Math
+    {
+        public void Add(IConsole console, int x, int y) => console.WriteLine(x + y);
+        public void AddRange(IConsole console, IEnumerable<int> values) => console.WriteLine(values.Sum());
+        public void Subtract(IConsole console, int x, int y) => console.WriteLine(x - y);
+    }
+}
