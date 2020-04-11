a5a5bb954d29377a756ac7e21cfacf22f50aecec
Drew Burlingame <drewburlingame@gmail.com>
2020-04-11 07:54:59 +0100

    remove obsoleted TestAppSettings.TestDefault
    version is no longer enabled by default

--- CommandDotNet.Tests/TestAppSettings.cs
+++ CommandDotNet.Tests/TestAppSettings.cs
@@ -8,13 +8,6 @@ namespace CommandDotNet.Tests
 {
     public static class TestAppSettings
     {
-        /// <summary>
-        /// version is a feature that's enabled for default. 
-        /// disabling it for tests reduces the noise in help expectations
-        /// and prevents tight coupling to the feature
-        /// </summary>
-        public static AppSettings TestDefault => new AppSettings();
-
         public static AppSettings BasicHelp => new AppSettings {Help = {TextStyle = HelpTextStyle.Basic}};
         public static AppSettings DetailedHelp => new AppSettings {Help = {TextStyle = HelpTextStyle.Detailed}};
 
