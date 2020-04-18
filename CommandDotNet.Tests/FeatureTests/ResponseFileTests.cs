66a0a371113a929216ba485aaab5a91b23c184a5
Drew Burlingame <drewburlingame@gmail.com>
2020-04-18 21:02:36 +0100

    Gracefully handle FileNotFound for response files
    Change TokenizerPipeline to write errors to console and
    return ExitCodes.Error instead of throwing exception.
    
    Validate @ can be used in options and operands

--- CommandDotNet.Tests/FeatureTests/ResponseFileTests.cs
+++ CommandDotNet.Tests/FeatureTests/ResponseFileTests.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using CommandDotNet.TestTools;
 using CommandDotNet.TestTools.Scenarios;
-using CommandDotNet.Tokens;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
@@ -11,13 +10,12 @@ namespace CommandDotNet.Tests.FeatureTests
 {
     public class ResponseFileTests : IDisposable
     {
-        private readonly ITestOutputHelper _output;
         private readonly TempFiles _tempFiles;
 
         public ResponseFileTests(ITestOutputHelper output)
         {
-            _output = output;
-            _tempFiles = new TempFiles(_output.WriteLine);
+            Ambient.Output = output;
+            _tempFiles = new TempFiles(output.WriteLine);
         }
 
         public void Dispose()
@@ -31,14 +29,14 @@ namespace CommandDotNet.Tests.FeatureTests
             var responseFile = _tempFiles.CreateTempFile("--opt1 opt1value arg1value");
 
             new AppRunner<App>()
-                .Verify(_output, new Scenario
+                .Verify(new Scenario
                 {
-                    WhenArgs = $"Do @{responseFile}",
+                    When = {Args = $"Do @{responseFile}" },
                     Then =
                     {
                         Captured =
                         {
-                            new App.DoResult {arg1 = $"@{responseFile}"}
+                            new DoResult {arg1 = $"@{responseFile}"}
                         }
                     }
                 });
@@ -51,14 +49,14 @@ namespace CommandDotNet.Tests.FeatureTests
 
             new AppRunner<App>()
                 .UseResponseFiles()
-                .Verify(_output, new Scenario
+                .Verify(new Scenario
                 {
-                    WhenArgs = $"Do @{responseFile}",
+                    When = { Args = $"Do @{responseFile}" },
                     Then =
                     {
                         Captured =
                         {
-                            new App.DoResult
+                            new DoResult
                             {
                                 arg1 = "arg1value",
                                 opt1 = "opt1value"
@@ -75,14 +73,14 @@ namespace CommandDotNet.Tests.FeatureTests
 
             new AppRunner<App>()
                 .UseResponseFiles()
-                .Verify(_output, new Scenario
+                .Verify(new Scenario
                 {
-                    WhenArgs = $"Do @{responseFile}",
+                    When = { Args = $"Do @{responseFile}" },
                     Then =
                     {
                         Captured =
                         {
-                            new App.DoResult
+                            new DoResult
                             {
                                 arg1 = "arg1value",
                                 opt1 = "opt1value"
@@ -99,14 +97,14 @@ namespace CommandDotNet.Tests.FeatureTests
 
             new AppRunner<App>()
                 .UseResponseFiles()
-                .Verify(_output, new Scenario
+                .Verify(new Scenario
                 {
-                    WhenArgs = $"Do arg1value @{responseFile}",
+                    When = { Args = $"Do arg1value @{responseFile}" },
                     Then =
                     {
                         Captured =
                         {
-                            new App.DoResult
+                            new DoResult
                             {
                                 arg1 = "arg1value",
                                 opt1 = "opt1value"
@@ -116,22 +114,89 @@ namespace CommandDotNet.Tests.FeatureTests
                 });
         }
 
-        // TEST only Arguments, not Directives or Separated
-
         [Fact]
-        public void OutputsClearErrorWhenFileNotFound()
+        public void Operand_ValueBeginningWithAmpersand_CanBeEscapedWith_EndOfOptions_ArgumentSeparator()
         {
-            var appRunner = new AppRunner<App>().UseResponseFiles();
-            var ex = Assert.Throws<TokenTransformationException>(() => 
-                appRunner.Run("Do", "@not-exists"));
-
-            ex.Message.Should().Contain("expand-response-file");
-            ex.InnerException.Should().BeOfType<FileNotFoundException>();
-            var fileName = ((FileNotFoundException)ex.InnerException).FileName;
-            Path.GetFileName(fileName).Should().Be("not-exists");
+            var endOfOptions = new AppSettings{DefaultArgumentSeparatorStrategy = ArgumentSeparatorStrategy.EndOfOptions};
+            new AppRunner<App>(endOfOptions)
+                .UseResponseFiles()
+                .Verify(new Scenario
+                {
+                    When = {Args = "Do -- @some-value"},
+                    Then =
+                    {
+                        Captured = {new DoResult {arg1 = "@some-value"}},
+                        AssertContext = c =>
+                            c.ParseResult.SeparatedArguments.Should().Contain("@some-value")
+                    }
+                });
         }
 
-        public class App
+        [Fact]
+        public void Option_ValueBeginningWithAmpersand_CanBeEscapedWith_ColonValueAssignment()
+        {
+            new AppRunner<App>()
+                .UseResponseFiles()
+                .Verify(new Scenario
+                {
+                    When = { Args = "Do --opt1:@some-value" },
+                    Then =
+                    {
+                        Captured = {new DoResult {opt1 = "@some-value"}}
+                    }
+                });
+        }
+
+        [Fact]
+        public void Option_ValueBeginningWithAmpersand_CanBeEscapedWith_EqualsValueAssignment()
+        {
+            new AppRunner<App>()
+                .UseResponseFiles()
+                .Verify(new Scenario
+                {
+                    When = { Args = "Do --opt1=@some-value" },
+                    Then =
+                    {
+                        Captured = {new DoResult {opt1 = "@some-value"}}
+                    }
+                });
+        }
+
+        [Fact]
+        public void WhenFileNotFound_Exit_Code1_And_InformativeErrorMessage_And_LogError()
+        {
+            List<string> logs = new List<string>();
+
+            new AppRunner<App>()
+                .UseResponseFiles()
+                .Verify(new Scenario
+                {
+                    When = { Args = "Do @not-exists.rsp" },
+                    Then =
+                    {
+                        ExitCode = 1,
+                        AssertOutput = o =>
+                        {
+                            o.Should().StartWith("File not found: ");
+                            o.TrimEnd(Environment.NewLine.ToCharArray())
+                                .Should().EndWith("not-exists.rsp");
+                        }
+                    }
+                }, logLine: line =>
+                {
+                    logs.Add(line);
+                    Ambient.Output.WriteLine(line);
+                }, TestConfig.Default.Where(c => c.PrintCommandDotNetLogs = true));
+
+            // the E indicates error level log
+            logs.Should().Contain(log =>
+                log.StartsWith(
+                    "E CommandDotNet.Tokens.TokenizerPipeline > Tokenizer error: TokenTransformation: expand-response-files")
+                && log.Contains("File not found")
+                && log.Contains("not-exists.rsp"));
+        }
+
+        private class App
         {
             private TestCaptures TestCaptures { get; set; }
 
@@ -139,12 +204,12 @@ namespace CommandDotNet.Tests.FeatureTests
             {
                 TestCaptures.Capture(new DoResult { opt1 = opt1, arg1 = arg1 });
             }
+        }
 
-            public class DoResult
-            {
-                public string opt1;
-                public string arg1;
-            }
+        public class DoResult
+        {
+            public string opt1;
+            public string arg1;
         }
     }
 }
