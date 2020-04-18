3f02b7d69d05ebb61a1d072ca59f7a07c16491a8
Drew Burlingame <drewburlingame@gmail.com>
2020-04-18 11:15:21 +0100

    Add CommandDotNet.Example.Tests proj w/ NUnit
    This proves out how TestTools works with both runners
    
    Also gives us a path to make the examples reliable

---
+++ CommandDotNet.Example.Tests/ExampleCommandTests.cs
@@ -0,0 +1,56 @@
+using CommandDotNet.TestTools.Scenarios;
+using NUnit.Framework;
+
+namespace CommandDotNet.Example.Tests
+{
+    [TestFixture]
+    public class ExampleCommandTests
+    {
+        [Test]
+        public void Help_Should_Include_ExtendedHelpText()
+        {
+            // Must convert NewLine to what was given
+            // to the ExtendedHelpText property
+            Program.GetAppRunner()
+                .Verify(new Scenario
+                {
+                    When = {Args = null},
+                    Then =
+                    {
+                        OutputContainsTexts =
+                        {
+                            @"
+Directives:
+  [debug] to attach a debugger to the app
+  [parse] to output how the inputs were tokenized
+  [log] to output framework logs to the console
+  [log:debug|info|warn|error|fatal] to output framework logs for the given level or above
+
+directives must be specified before any commands and arguments.
+
+Example: testhost.exe [debug] [parse] [log:info] cancel-me".Replace("\r\n", "\n")
+                        }
+                    }
+                });
+        }
+
+        [Test]
+        public void Should_Include_Version_Option()
+        {
+            // The directives section from the command must match
+            Program.GetAppRunner()
+                .Verify(new Scenario
+                {
+                    When = { Args = null },
+                    Then =
+                    {
+                        OutputContainsTexts =
+                        {
+                            @"-v | --version
+  Show version information",
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ CommandDotNet.Example.Tests/PipedInputTests.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using CommandDotNet.Rendering;
+using CommandDotNet.TestTools;
+using CommandDotNet.TestTools.Scenarios;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CommandDotNet.Example.Tests
+{
+    [TestFixture]
+    public class PipedInputTests
+    {
+        [Test]
+        public void PipedInput_Should_UnionWithUserSuppliedValues()
+        {
+            var result = new AppRunner<App>()
+                .AppendPipedInputToOperandList()
+                .RunInMem("List aaa bbb", pipedInput: new[] { "ccc", "ddd" });
+
+            result.ExitCode.Should().Be(0);
+            result.OutputShouldBe(@"aaa
+bbb
+ccc
+ddd
+");
+        }
+
+        [Test]
+        public void PipedInput_Should_UnionWithUserSuppliedValues_BDD()
+        {
+            new AppRunner<App>()
+                .AppendPipedInputToOperandList()
+                .Verify(new Scenario
+                {
+                    When=
+                    {
+                        Args = "List aaa bbb",
+                        PipedInput = new[] { "ccc", "ddd" }
+                    },
+                    Then =
+                    {
+                        Output = @"aaa
+bbb
+ccc
+ddd
+"
+                    }
+                });
+        }
+
+        private class App
+        {
+            public void List(IConsole console, List<string> args) =>
+                console.WriteLine(string.Join(Environment.NewLine, args));
+        }
+    }
+}
