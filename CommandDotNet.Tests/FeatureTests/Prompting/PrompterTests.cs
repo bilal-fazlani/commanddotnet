5b7c11bc8782d56112dcb5387fd19cd28ff8edff
Stanislav Trifan <trifanstanislav@gmail.com>
2020-05-01 16:37:13 +0300

    Added few additional tests for prompting. Not able to run these tests on Ubuntu 18.04 with dotnet 3.1.201 in Rider 2020.1

--- CommandDotNet.Tests/FeatureTests/Prompting/PrompterTests.cs
+++ CommandDotNet.Tests/FeatureTests/Prompting/PrompterTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
 using CommandDotNet.Prompts;
 using CommandDotNet.Rendering;
 using CommandDotNet.TestTools.Prompts;
@@ -36,12 +36,56 @@ who's there
                 });
         }
 
+        [Fact]
+        public void PromptForArguments()
+        {
+            new AppRunner<App>()
+                .UsePrompting()
+                .Verify(new Scenario
+                {
+                    When =
+                    {
+                        Args = "Greeting",
+                        OnPrompt = Respond.WithText("world")
+                    },
+                    Then =
+                    {
+                        OutputContainsTexts = new List<string> {"Hello world"}
+                    }
+                });
+        }
+
+
+        [Fact]
+        public void PromptForSpecialArguments()
+        {
+            new AppRunner<App>()
+                .UsePrompting()
+                .Verify(new Scenario
+                {
+                    When =
+                    {
+                        Args = "Greeting",
+                        OnPrompt = Respond.WithText("some: [yaml, sample]")
+                    },
+                    Then =
+                    {
+                        OutputContainsTexts = new List<string> {"Hello some: [yaml, sample]"}
+                    }
+                });
+        }
+
         public class App
         {
             public void Do(IConsole console, IPrompter prompter)
             {
                 var answer = prompter.PromptForValue("knock knock", out bool isCancellationRequested);
                 console.Out.WriteLine(answer);
+            }
+
+            public void Greeting(string drWho, IConsole console)
+            {
+                console.Out.WriteLine($"Hello {drWho}");
             }
         }
     }
