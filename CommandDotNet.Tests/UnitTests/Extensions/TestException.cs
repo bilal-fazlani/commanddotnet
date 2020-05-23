c387c759bd6ff0b802ac7f510d8098fd8749cb96
Drew Burlingame <drewburlingame@gmail.com>
2020-05-23 08:59:31 +0100

    Special handling to print exceptions w/ properties & data
    - adding Ex.Print extension methods to print exceptions with
    properties, data, stack trace & consistent indents so they
    can be included when printing stuff like AppConfig and
    logging errors in unit tests
    
    - Object.ToIndentedString uses the new Ex print
    to include properties and data

---
+++ CommandDotNet.Tests/UnitTests/Extensions/TestException.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CommandDotNet.Tests.UnitTests.Extensions
+{
+    public class TestException : Exception
+    {
+        private static TestException? instance;
+            
+        public string SomeProperty => "Some property value";
+
+        public static TestException Instance => instance ??= Build();
+
+        public TestException() : base("I'm a test exception")
+        {
+            base.Data.Add("data-key", "data value");
+        }
+
+        private static TestException Build()
+        {
+            try
+            {
+                ThrowEx();
+                
+                // this line is never called but required for compiler
+                return new TestException();
+            }
+            catch (TestException e)
+            {
+                return e;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void ThrowEx()
+        {
+            throw new TestException();
+        }
+    }
+}
