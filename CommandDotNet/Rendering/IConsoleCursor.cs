8affebd6246128a16b5e9b71b268d646015d37ed
Drew Burlingame <drewburlingame@gmail.com>
2020-06-09 19:35:46 +0100

    IConsole,SystemConsole,TestConsole add most Console members
    TestConsole
    - TestConsole.Default contains default values
    - all new members are just pass-thru for now, without
      special logic

---
+++ CommandDotNet/Rendering/IConsoleCursor.cs
@@ -0,0 +1,11 @@
+﻿namespace CommandDotNet.Rendering
+{
+    public interface IConsoleCursor
+    {
+        int CursorSize { get; set; }
+        bool CursorVisible { get; set; }
+        int CursorLeft { get; set; }
+        int CursorTop { get; set; }
+        void SetCursorPosition(int left, int top);
+    }
+}
