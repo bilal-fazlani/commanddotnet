d55e4e56d8cfa810b365639f011490b369aa37ce
Drew Burlingame <drewburlingame@gmail.com>
2021-11-16 16:05:02 +0000

    move IConsole to root NS for ease and discoverability
    while technically a breaking change, it's unlikely to break
    much since most files using this will already have a using
    for CommandDotNet

---
+++ CommandDotNet/Rendering/IStandardOut.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace CommandDotNet.Rendering
+{
+    public interface IStandardOut
+    {
+        TextWriter Out { get; }
+        
+        void SetOut(TextWriter newOut);
+
+        bool IsOutputRedirected { get; }
+    }
+}
