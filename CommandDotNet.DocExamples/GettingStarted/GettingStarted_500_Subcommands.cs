3c78d057139c63a6b734cdc6cf57a62c333a7f05
Drew Burlingame <drewburlingame@gmail.com>
2021-12-13 11:08:48 +0000

    fix triplicate snippets in getting started tests page

--- CommandDotNet.DocExamples/GettingStarted/GettingStarted_500_Subcommands.cs
+++ CommandDotNet.DocExamples/GettingStarted/GettingStarted_500_Subcommands.cs
@@ -50,7 +50,6 @@ namespace CommandDotNet.DocExamples.GettingStarted
         [TestFixture]
         public class AddCommandTests
         {
-            // begin-snippet: getting-started-400-calculator-add-command-tests
             [Test]
             public void Given2Numbers_Should_OutputSum()
             {
@@ -58,13 +57,11 @@ namespace CommandDotNet.DocExamples.GettingStarted
                 result.ExitCode.Should().Be(0);
                 result.Console.OutText().Should().Be("60");
             }
-            // end-snippet
         }
 
         [TestFixture]
         public class AddCommandTestsBDD
         {
-            // begin-snippet: getting-started-400-calculator-add-command-tests-bdd
             [Test]
             public void Given2Numbers_Should_OutputSum() =>
                 Program.AppRunner.Verify(new Scenario
@@ -84,7 +81,6 @@ namespace CommandDotNet.DocExamples.GettingStarted
                         Output = "'a' is not a valid Number"
                     }
                 });
-            // end-snippet
         }
     }
 }
