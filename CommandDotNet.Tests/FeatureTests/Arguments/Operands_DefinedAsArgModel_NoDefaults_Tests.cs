d42e222bfb873a1b0c70a7d10bb59b79e6e8c201
Drew Burlingame <drewburlingame@gmail.com>
2021-12-20 20:18:37 +0000

    remove old tests now covered by arity tests

--- CommandDotNet.Tests/FeatureTests/Arguments/Operands_DefinedAsArgModel_NoDefaults_Tests.cs
+++ CommandDotNet.Tests/FeatureTests/Arguments/Operands_DefinedAsArgModel_NoDefaults_Tests.cs
@@ -222,46 +222,6 @@ Arguments:
         }
 
         [Fact]
-        public void SampleTypes_Exec_OperandsNotRequired()
-        {
-            new AppRunner<OperandsNoDefaults>().Verify(new Scenario
-            {
-                When = {Args = nameof(OperandsNoDefaults.ArgsNoDefault)},
-                Then =
-                {
-                    ExitCode = 2,
-                    Output = @"StringArg is required
-ObjectArg is required
-StringListArg is required",
-                    AssertContext = ctx => ctx.ParamValuesShouldBe(
-                        new OperandsNoDefaultsSampleTypesModel
-                        {
-                            StructArg = default,
-                            EnumArg = default,
-                        })
-                }
-            });
-        }
-
-        [Fact]
-        public void SampleTypes_Exec_OptionalOperandsNotRequired()
-        {
-            new AppRunner<OperandsNoDefaults>().Verify(new Scenario
-            {
-                When = { Args = nameof(OperandsNoDefaults.NrtArgsNoDefault) },
-                Then =
-                {
-                    AssertContext = ctx => ctx.ParamValuesShouldBe(
-                        new OperandsNoDefaultsSampleTypesModel
-                        {
-                            StructArg = default,
-                            EnumArg = default,
-                        })
-                }
-            });
-        }
-
-        [Fact]
         public void StructList_Exec_Positional()
         {
             new AppRunner<OperandsNoDefaults>().Verify(new Scenario
