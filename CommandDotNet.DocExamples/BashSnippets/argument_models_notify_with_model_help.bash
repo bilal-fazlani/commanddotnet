6e89298ae272371acfba236b063654178afa3191
Drew Burlingame <drewburlingame@gmail.com>
2021-12-21 22:32:00 +0000

    update argument models docs

---
+++ CommandDotNet.DocExamples/BashSnippets/argument_models_notify_with_model_help.bash
@@ -0,0 +1,18 @@
+// begin-snippet: argument_models_notify_with_model_help
+$ myapp.exe Notify --help
+Usage: myapp.exe Notify [options] <Message> <Recipients>
+
+Arguments:
+
+  Message                <TEXT>
+
+  Recipients (Multiple)  <TEXT>
+
+Options:
+
+  --dryrun
+
+  -v | --Verbose
+
+  -q | --Quite
+// end-snippet
---
+++ CommandDotNet.DocExamples/BashSnippets/argument_models_notify_without_model_help.bash
@@ -0,0 +1,18 @@
+// begin-snippet: argument_models_notify_without_model_help
+$ myapp.exe Notify --help
+Usage: myapp.exe Notify [options] <message> <recipients>
+
+Arguments:
+
+  message                <TEXT>
+
+  recipients (Multiple)  <TEXT>
+
+Options:
+
+  --dryrun
+
+  -v | --verbose
+
+  -q | --quiet
+// end-snippet
