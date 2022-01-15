2b4bc6b00acc50faa01b60a95a59419f8d33fd15
Drew Burlingame <drewburlingame@gmail.com>
2022-01-15 23:49:13 +0000

    update release notes for CommandDotNet 6.0.3

--- CommandDotNet/CommandContextExtensions.cs
+++ CommandDotNet/CommandContextExtensions.cs
@@ -3,7 +3,7 @@
     public static class CommandContextExtensions
     {
         /// <summary>
-        /// Prints help for the provided command if provided, else the target command if exists, else the root command.
+        /// Prints help for a provided command, else the target command if parsed, else the root command.
         /// </summary>
         public static void PrintHelp(this CommandContext commandContext, Command? command = null)
         {
